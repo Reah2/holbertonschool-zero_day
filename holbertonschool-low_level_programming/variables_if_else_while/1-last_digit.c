59ae98eb6826a639efaf02b2ec709a82059415e2
Reah2 <jamesramoshabe5@gmail.com>
2023-10-27 01:20:49 -0700

    last digit file commit

---
+++ holbertonschool-low_level_programming/variables_if_else_while/1-last_digit.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+/**
+ *main - Entry point
+ *Return: Always 0
+ *
+ */
+
+int main(void)
+	
+{
+
+int n;
+int last_digit;
+
+/* Initialize random number generator with the current time */
+
+srand(time(0));
+
+/* Generate a random number and store it in n */
+
+n = rand();
+
+/* Extract the last digit of n */
+
+last_digit = n % 10;
+
+if (last_digit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+}
+else if (last_digit == 0)
+{
+printf("Last digit of %d is %d and is 0\n" , n, last_digit);						   }
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n" , n, last_digit);
+}
+return (0);
+}
